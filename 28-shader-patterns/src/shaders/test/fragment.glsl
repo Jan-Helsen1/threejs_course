81ff17525bca1ad4623caeee129f5e0856984de6
Jan-Helsen1 <r0795805@ucll.be>
2023-04-02 14:27:25 +0200

    les 29 deels

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -227,8 +227,8 @@ void main()
 
     // // Pattern 39
     // vec2 wavedUv = vec2(
-    //     vUv.x + sin(vUv.y * 100.0) * 0.1,
-    //     vUv.y + sin(vUv.x * 100.0) * 0.1
+    //     vUv.x + sin(vUv.y * 200.0) * 0.1,
+    //     vUv.y + sin(vUv.x * 200.0) * 0.1
     // );   
 
     // float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
